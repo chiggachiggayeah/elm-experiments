77faa1cd3b5d24429e477f86083c93c67cd2b604
Jordan <jordankhall23@gmail.com>
2015-12-09 21:11:17 -0500

    random gen, and strings

--- balloons/balloons.elm
+++ balloons/balloons.elm
@@ -1,58 +1,197 @@
+{-
+  In trying to learn elm, I saw a few things utilizing physics.
+
+  With the nature of signals, something requiring a system of regular updateLoons
+  seemed interesting so I thought I would give it a go.
+
+  Goals:
+    [x] Vertical movement
+    [x] Random Gen (Color, Pos)
+    [] Collisions and Horizontal movement
+    [] String
+    [] Mouse interaction, popping
+-}
+
 import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
+import List exposing (foldr, map)
+import Text exposing (..)
+import Random exposing (..)
+import Window
 import Time exposing (..)
 
 -- Model
 type alias Loon =
   { x: Float
   , y: Float
+  , vx: Float
+  , vy: Float
   , b: Form
   }
 
+type alias Sim =
+  { allLoons : List Loon
+  , deltaSum : Float
+  }
+
+baseHeight = 500
+baseWidth = 500
+
+radius : Float
+radius = baseWidth / 10
+
+-- type Params =
+
+getHeight : Int -> Float
+getHeight h = toFloat h
+
+runGen : Seed -> (Int, Seed)
+runGen seed =
+  let
+    gen = int -255 255
+  in
+    generate gen seed
+
+genInitSeed : Seed
+genInitSeed = initialSeed 36758
+
+getSeed : (Int, Seed) -> Seed
+getSeed (i, s) = s
+
+getInt : (Int, Seed) -> Int
+getInt (i, s) = i
+
 balloon : (Float, Float) -> Form
 balloon (x, y) =
-  group
-    [ circle 75
-        |> filled charcoal
-        |> move(x, y)
-      ,ngon 3 81
-        |> filled charcoal
-        |> move (x, -68 + y)
-        |> rotate (degrees -90)
-    ]
+  let
+    newColor = (rgba
+                (floor (abs x))
+                (floor (abs (y + baseHeight)))
+                (min 75 (floor (abs (x + y + baseHeight))))
+                1.0)
+    ribbon = traced (solid red) (path [(x, y - (radius * 7)), (x, y - radius * 2)])
+  in
+    group
+      [ circle radius
+          |> filled newColor
+          |> move(x, y)
+        ,ngon 3 radius
+          |> filled newColor
+          |> move (x, -50 + y)
+          |> rotate (degrees -90)
+        ,ribbon
+      ]
 
-
-bShape = balloon (-20, -100)
+bShape = balloon (-20, 0)
 
 aBalloon : Loon
 aBalloon =
   { x =  -20
-  , y =  -100
+  , y =  0
   , b =  bShape
+  , vx = 0
+  , vy = 0
   }
 
-view : Loon -> Element
-view loon = collage 1000 1000 [ loon.b ]
+bBalloon : Loon
+bBalloon =
+  { x = 70
+  , y = 10
+  , b = balloon (80, 10)
+  , vx = 0
+  , vy = 0
+  }
 
-update : Float -> Loon -> Loon
-update dt loon =
-  if (loon.y + (dt/0.9)) < 500 then
+simulation : Sim
+simulation =
+  { allLoons = [ aBalloon, bBalloon ]
+  , deltaSum = 0
+  }
+
+-- control how the shapes are affected visually
+toTuple {x, y} = (x, y)
+
+
+view : (Int, Int) -> Sim -> Element
+view (w', h') sim =
+  let
+    gameScale = min (toFloat h' / baseHeight) 1
+    allLoons = (List.map .b sim.allLoons)
+  in
+    collage w' h' (List.map (scale gameScale) allLoons)
+
+
+-- compute the physics on the model
+updateLoon : Float -> Loon -> Loon
+updateLoon dt loon =
+  let
+    delta = (dt / 100)
+    -- h = Signal.map getHeight  Window.dimensions
+  in
     { loon |
-      b = loon.b |> moveY (dt/0.9),
-      y = loon.y + (dt/0.9)
+        x = loon.x,
+        y = if loon.y < (baseHeight - (radius * 3))
+              then
+                loon.y + loon.vy
+              else
+                loon.y,
+        vx = loon.vx,
+        vy = if loon.vy < 20 then loon.vy + delta else loon.vy,
+        b = if (loon.y) < (baseHeight - (radius * 3))
+              then
+                moveY (loon.vy) loon.b
+              else
+                loon.b
     }
-  else
-    loon
+
+updateLoons : Float -> Sim -> Sim
+updateLoons dt sim =
+  let
+    -- append to loons
+    -- random color
+    -- loons = sim.allLoons
+    balloons =
+      let
+        thisXSeed = initialSeed (floor sim.deltaSum)
+        thisYSeed = initialSeed (ceiling (sim.deltaSum * 5))
+        randXVal = getInt ( runGen thisXSeed )
+        randYVal = getInt ( runGen thisYSeed )
+        newLoon : Loon
+        newLoon =
+          { x = toFloat (randXVal)
+          , y = toFloat (randYVal - baseHeight)
+          , vx = 0
+          , vy = 0
+          , b = balloon(toFloat(randXVal), toFloat(randYVal - baseHeight))
+          }
+      in
+        -- newLoon :: loons
+        if (sim.deltaSum / toFloat (List.length sim.allLoons)) > 30  then newLoon :: sim.allLoons else sim.allLoons
+  in
+    { sim |
+        allLoons = (List.map (updateLoon dt) balloons),
+        deltaSum = sim.deltaSum + dt
+    }
+
+
+showLoons sim = show sim
+
 
 iter : Signal Float
-iter = Signal.map (\t -> t/20) (fps 30)
+iter = Signal.map (\t -> t/50) (fps 60)
+
+toPair : Loon -> Float -> (Loon, Float)
+toPair loon fl = (loon, fl)
+
+simStep : Signal Sim
+simStep =  (Signal.foldp updateLoons simulation iter)
+
+runSim : Signal Element
+runSim = Signal.map2 view Window.dimensions simStep
+
+showSim : Signal Element
+showSim = Signal.map showLoons simStep
 
 main : Signal Element
-main = Signal.map view (Signal.foldp update aBalloon iter)
-
-
-
-
--- Update
--- View
+main = runSim
--- balloons/elm.js
+++ balloons/elm.js
@@ -5577,6 +5577,313 @@ Elm.Time.make = function (_elm) {
                              ,delay: delay
                              ,since: since};
 };
+Elm.Native = Elm.Native || {};
+Elm.Native.Window = {};
+Elm.Native.Window.make = function make(localRuntime) {
+	localRuntime.Native = localRuntime.Native || {};
+	localRuntime.Native.Window = localRuntime.Native.Window || {};
+	if (localRuntime.Native.Window.values)
+	{
+		return localRuntime.Native.Window.values;
+	}
+
+	var NS = Elm.Native.Signal.make(localRuntime);
+	var Tuple2 = Elm.Native.Utils.make(localRuntime).Tuple2;
+
+
+	function getWidth()
+	{
+		return localRuntime.node.clientWidth;
+	}
+
+
+	function getHeight()
+	{
+		if (localRuntime.isFullscreen())
+		{
+			return window.innerHeight;
+		}
+		return localRuntime.node.clientHeight;
+	}
+
+
+	var dimensions = NS.input('Window.dimensions', Tuple2(getWidth(), getHeight()));
+
+
+	function resizeIfNeeded()
+	{
+		// Do not trigger event if the dimensions have not changed.
+		// This should be most of the time.
+		var w = getWidth();
+		var h = getHeight();
+		if (dimensions.value._0 === w && dimensions.value._1 === h)
+		{
+			return;
+		}
+
+		setTimeout(function() {
+			// Check again to see if the dimensions have changed.
+			// It is conceivable that the dimensions have changed
+			// again while some other event was being processed.
+			w = getWidth();
+			h = getHeight();
+			if (dimensions.value._0 === w && dimensions.value._1 === h)
+			{
+				return;
+			}
+			localRuntime.notify(dimensions.id, Tuple2(w, h));
+		}, 0);
+	}
+
+
+	localRuntime.addListener([dimensions.id], window, 'resize', resizeIfNeeded);
+
+
+	return localRuntime.Native.Window.values = {
+		dimensions: dimensions,
+		resizeIfNeeded: resizeIfNeeded
+	};
+};
+
+Elm.Random = Elm.Random || {};
+Elm.Random.make = function (_elm) {
+   "use strict";
+   _elm.Random = _elm.Random || {};
+   if (_elm.Random.values) return _elm.Random.values;
+   var _U = Elm.Native.Utils.make(_elm),$Basics = Elm.Basics.make(_elm),$List = Elm.List.make(_elm);
+   var _op = {};
+   var magicNum8 = 2147483562;
+   var range = function (_p0) {    return {ctor: "_Tuple2",_0: 0,_1: magicNum8};};
+   var magicNum7 = 2137383399;
+   var magicNum6 = 2147483563;
+   var magicNum5 = 3791;
+   var magicNum4 = 40692;
+   var magicNum3 = 52774;
+   var magicNum2 = 12211;
+   var magicNum1 = 53668;
+   var magicNum0 = 40014;
+   var generate = F2(function (_p1,seed) {    var _p2 = _p1;return _p2._0(seed);});
+   var Seed = function (a) {    return {ctor: "Seed",_0: a};};
+   var State = F2(function (a,b) {    return {ctor: "State",_0: a,_1: b};});
+   var initState = function (s$) {
+      var s = A2($Basics.max,s$,0 - s$);
+      var q = s / (magicNum6 - 1) | 0;
+      var s2 = A2($Basics._op["%"],q,magicNum7 - 1);
+      var s1 = A2($Basics._op["%"],s,magicNum6 - 1);
+      return A2(State,s1 + 1,s2 + 1);
+   };
+   var next = function (_p3) {
+      var _p4 = _p3;
+      var _p6 = _p4._1;
+      var _p5 = _p4._0;
+      var k$ = _p6 / magicNum3 | 0;
+      var s2$ = magicNum4 * (_p6 - k$ * magicNum3) - k$ * magicNum5;
+      var s2$$ = _U.cmp(s2$,0) < 0 ? s2$ + magicNum7 : s2$;
+      var k = _p5 / magicNum1 | 0;
+      var s1$ = magicNum0 * (_p5 - k * magicNum1) - k * magicNum2;
+      var s1$$ = _U.cmp(s1$,0) < 0 ? s1$ + magicNum6 : s1$;
+      var z = s1$$ - s2$$;
+      var z$ = _U.cmp(z,1) < 0 ? z + magicNum8 : z;
+      return {ctor: "_Tuple2",_0: z$,_1: A2(State,s1$$,s2$$)};
+   };
+   var split = function (_p7) {
+      var _p8 = _p7;
+      var _p11 = _p8._1;
+      var _p10 = _p8._0;
+      var _p9 = $Basics.snd(next(_p8));
+      var t1 = _p9._0;
+      var t2 = _p9._1;
+      var new_s2 = _U.eq(_p11,1) ? magicNum7 - 1 : _p11 - 1;
+      var new_s1 = _U.eq(_p10,magicNum6 - 1) ? 1 : _p10 + 1;
+      return {ctor: "_Tuple2",_0: A2(State,new_s1,t2),_1: A2(State,t1,new_s2)};
+   };
+   var initialSeed = function (n) {    return Seed({state: initState(n),next: next,split: split,range: range});};
+   var Generator = function (a) {    return {ctor: "Generator",_0: a};};
+   var andThen = F2(function (_p12,callback) {
+      var _p13 = _p12;
+      return Generator(function (seed) {
+         var _p14 = _p13._0(seed);
+         var result = _p14._0;
+         var newSeed = _p14._1;
+         var _p15 = callback(result);
+         var genB = _p15._0;
+         return genB(newSeed);
+      });
+   });
+   var map5 = F6(function (func,_p20,_p19,_p18,_p17,_p16) {
+      var _p21 = _p20;
+      var _p22 = _p19;
+      var _p23 = _p18;
+      var _p24 = _p17;
+      var _p25 = _p16;
+      return Generator(function (seed0) {
+         var _p26 = _p21._0(seed0);
+         var a = _p26._0;
+         var seed1 = _p26._1;
+         var _p27 = _p22._0(seed1);
+         var b = _p27._0;
+         var seed2 = _p27._1;
+         var _p28 = _p23._0(seed2);
+         var c = _p28._0;
+         var seed3 = _p28._1;
+         var _p29 = _p24._0(seed3);
+         var d = _p29._0;
+         var seed4 = _p29._1;
+         var _p30 = _p25._0(seed4);
+         var e = _p30._0;
+         var seed5 = _p30._1;
+         return {ctor: "_Tuple2",_0: A5(func,a,b,c,d,e),_1: seed5};
+      });
+   });
+   var map4 = F5(function (func,_p34,_p33,_p32,_p31) {
+      var _p35 = _p34;
+      var _p36 = _p33;
+      var _p37 = _p32;
+      var _p38 = _p31;
+      return Generator(function (seed0) {
+         var _p39 = _p35._0(seed0);
+         var a = _p39._0;
+         var seed1 = _p39._1;
+         var _p40 = _p36._0(seed1);
+         var b = _p40._0;
+         var seed2 = _p40._1;
+         var _p41 = _p37._0(seed2);
+         var c = _p41._0;
+         var seed3 = _p41._1;
+         var _p42 = _p38._0(seed3);
+         var d = _p42._0;
+         var seed4 = _p42._1;
+         return {ctor: "_Tuple2",_0: A4(func,a,b,c,d),_1: seed4};
+      });
+   });
+   var map3 = F4(function (func,_p45,_p44,_p43) {
+      var _p46 = _p45;
+      var _p47 = _p44;
+      var _p48 = _p43;
+      return Generator(function (seed0) {
+         var _p49 = _p46._0(seed0);
+         var a = _p49._0;
+         var seed1 = _p49._1;
+         var _p50 = _p47._0(seed1);
+         var b = _p50._0;
+         var seed2 = _p50._1;
+         var _p51 = _p48._0(seed2);
+         var c = _p51._0;
+         var seed3 = _p51._1;
+         return {ctor: "_Tuple2",_0: A3(func,a,b,c),_1: seed3};
+      });
+   });
+   var map2 = F3(function (func,_p53,_p52) {
+      var _p54 = _p53;
+      var _p55 = _p52;
+      return Generator(function (seed0) {
+         var _p56 = _p54._0(seed0);
+         var a = _p56._0;
+         var seed1 = _p56._1;
+         var _p57 = _p55._0(seed1);
+         var b = _p57._0;
+         var seed2 = _p57._1;
+         return {ctor: "_Tuple2",_0: A2(func,a,b),_1: seed2};
+      });
+   });
+   var map = F2(function (func,_p58) {
+      var _p59 = _p58;
+      return Generator(function (seed0) {    var _p60 = _p59._0(seed0);var a = _p60._0;var seed1 = _p60._1;return {ctor: "_Tuple2",_0: func(a),_1: seed1};});
+   });
+   var listHelp = F4(function (list,n,generate,seed) {
+      listHelp: while (true) if (_U.cmp(n,1) < 0) return {ctor: "_Tuple2",_0: $List.reverse(list),_1: seed}; else {
+            var _p61 = generate(seed);
+            var value = _p61._0;
+            var newSeed = _p61._1;
+            var _v19 = A2($List._op["::"],value,list),_v20 = n - 1,_v21 = generate,_v22 = newSeed;
+            list = _v19;
+            n = _v20;
+            generate = _v21;
+            seed = _v22;
+            continue listHelp;
+         }
+   });
+   var list = F2(function (n,_p62) {    var _p63 = _p62;return Generator(function (seed) {    return A4(listHelp,_U.list([]),n,_p63._0,seed);});});
+   var pair = F2(function (genA,genB) {    return A3(map2,F2(function (v0,v1) {    return {ctor: "_Tuple2",_0: v0,_1: v1};}),genA,genB);});
+   var minInt = -2147483648;
+   var maxInt = 2147483647;
+   var iLogBase = F2(function (b,i) {    return _U.cmp(i,b) < 0 ? 1 : 1 + A2(iLogBase,b,i / b | 0);});
+   var $int = F2(function (a,b) {
+      return Generator(function (_p64) {
+         var _p65 = _p64;
+         var _p70 = _p65._0;
+         var base = 2147483561;
+         var f = F3(function (n,acc,state) {
+            f: while (true) {
+               var _p66 = n;
+               if (_p66 === 0) {
+                     return {ctor: "_Tuple2",_0: acc,_1: state};
+                  } else {
+                     var _p67 = _p70.next(state);
+                     var x = _p67._0;
+                     var state$ = _p67._1;
+                     var _v26 = n - 1,_v27 = x + acc * base,_v28 = state$;
+                     n = _v26;
+                     acc = _v27;
+                     state = _v28;
+                     continue f;
+                  }
+            }
+         });
+         var _p68 = _U.cmp(a,b) < 0 ? {ctor: "_Tuple2",_0: a,_1: b} : {ctor: "_Tuple2",_0: b,_1: a};
+         var lo = _p68._0;
+         var hi = _p68._1;
+         var k = hi - lo + 1;
+         var n = A2(iLogBase,base,k);
+         var _p69 = A3(f,n,1,_p70.state);
+         var v = _p69._0;
+         var state$ = _p69._1;
+         return {ctor: "_Tuple2",_0: lo + A2($Basics._op["%"],v,k),_1: Seed(_U.update(_p70,{state: state$}))};
+      });
+   });
+   var $float = F2(function (a,b) {
+      return Generator(function (seed) {
+         var _p71 = A2(generate,A2($int,minInt,maxInt),seed);
+         var number = _p71._0;
+         var newSeed = _p71._1;
+         var negativeOneToOne = $Basics.toFloat(number) / $Basics.toFloat(maxInt - minInt);
+         var _p72 = _U.cmp(a,b) < 0 ? {ctor: "_Tuple2",_0: a,_1: b} : {ctor: "_Tuple2",_0: b,_1: a};
+         var lo = _p72._0;
+         var hi = _p72._1;
+         var scaled = (lo + hi) / 2 + (hi - lo) * negativeOneToOne;
+         return {ctor: "_Tuple2",_0: scaled,_1: newSeed};
+      });
+   });
+   var bool = A2(map,F2(function (x,y) {    return _U.eq(x,y);})(1),A2($int,0,1));
+   return _elm.Random.values = {_op: _op
+                               ,bool: bool
+                               ,$int: $int
+                               ,$float: $float
+                               ,list: list
+                               ,pair: pair
+                               ,map: map
+                               ,map2: map2
+                               ,map3: map3
+                               ,map4: map4
+                               ,map5: map5
+                               ,andThen: andThen
+                               ,minInt: minInt
+                               ,maxInt: maxInt
+                               ,generate: generate
+                               ,initialSeed: initialSeed};
+};
+Elm.Window = Elm.Window || {};
+Elm.Window.make = function (_elm) {
+   "use strict";
+   _elm.Window = _elm.Window || {};
+   if (_elm.Window.values) return _elm.Window.values;
+   var _U = Elm.Native.Utils.make(_elm),$Basics = Elm.Basics.make(_elm),$Native$Window = Elm.Native.Window.make(_elm),$Signal = Elm.Signal.make(_elm);
+   var _op = {};
+   var dimensions = $Native$Window.dimensions;
+   var width = A2($Signal.map,$Basics.fst,dimensions);
+   var height = A2($Signal.map,$Basics.snd,dimensions);
+   return _elm.Window.values = {_op: _op,dimensions: dimensions,width: width,height: height};
+};
 Elm.Main = Elm.Main || {};
 Elm.Main.make = function (_elm) {
    "use strict";
@@ -5590,31 +5897,112 @@ Elm.Main.make = function (_elm) {
    $Graphics$Element = Elm.Graphics.Element.make(_elm),
    $List = Elm.List.make(_elm),
    $Maybe = Elm.Maybe.make(_elm),
+   $Random = Elm.Random.make(_elm),
    $Result = Elm.Result.make(_elm),
    $Signal = Elm.Signal.make(_elm),
-   $Time = Elm.Time.make(_elm);
+   $Time = Elm.Time.make(_elm),
+   $Window = Elm.Window.make(_elm);
    var _op = {};
-   var iter = A2($Signal.map,function (t) {    return t / 20;},$Time.fps(30));
-   var update = F2(function (dt,loon) {
-      return _U.cmp(loon.y + dt / 0.9,500) < 0 ? _U.update(loon,{b: A2($Graphics$Collage.moveY,dt / 0.9,loon.b),y: loon.y + dt / 0.9}) : loon;
-   });
-   var view = function (loon) {    return A3($Graphics$Collage.collage,1000,1000,_U.list([loon.b]));};
-   var balloon = function (_p0) {
-      var _p1 = _p0;
-      var _p3 = _p1._1;
-      var _p2 = _p1._0;
+   var toPair = F2(function (loon,fl) {    return {ctor: "_Tuple2",_0: loon,_1: fl};});
+   var iter = A2($Signal.map,function (t) {    return t / 50;},$Time.fps(60));
+   var showLoons = function (sim) {    return $Graphics$Element.show(sim);};
+   var toTuple = function (_p0) {    var _p1 = _p0;return {ctor: "_Tuple2",_0: _p1.x,_1: _p1.y};};
+   var getInt = function (_p2) {    var _p3 = _p2;return _p3._0;};
+   var getSeed = function (_p4) {    var _p5 = _p4;return _p5._1;};
+   var genInitSeed = $Random.initialSeed(36758);
+   var runGen = function (seed) {    var gen = A2($Random.$int,-255,255);return A2($Random.generate,gen,seed);};
+   var getHeight = function (h) {    return $Basics.toFloat(h);};
+   var baseWidth = 500;
+   var radius = baseWidth / 10;
+   var baseHeight = 500;
+   var balloon = function (_p6) {
+      var _p7 = _p6;
+      var _p9 = _p7._1;
+      var _p8 = _p7._0;
+      var ribbon = A2($Graphics$Collage.traced,
+      $Graphics$Collage.solid($Color.red),
+      $Graphics$Collage.path(_U.list([{ctor: "_Tuple2",_0: _p8,_1: _p9 - radius * 7},{ctor: "_Tuple2",_0: _p8,_1: _p9 - radius * 2}])));
+      var newColor = A4($Color.rgba,
+      $Basics.floor($Basics.abs(_p8)),
+      $Basics.floor($Basics.abs(_p9 + baseHeight)),
+      A2($Basics.min,75,$Basics.floor($Basics.abs(_p8 + _p9 + baseHeight))),
+      1.0);
       return $Graphics$Collage.group(_U.list([A2($Graphics$Collage.move,
-                                             {ctor: "_Tuple2",_0: _p2,_1: _p3},
-                                             A2($Graphics$Collage.filled,$Color.charcoal,$Graphics$Collage.circle(75)))
+                                             {ctor: "_Tuple2",_0: _p8,_1: _p9},
+                                             A2($Graphics$Collage.filled,newColor,$Graphics$Collage.circle(radius)))
                                              ,A2($Graphics$Collage.rotate,
                                              $Basics.degrees(-90),
                                              A2($Graphics$Collage.move,
-                                             {ctor: "_Tuple2",_0: _p2,_1: -68 + _p3},
-                                             A2($Graphics$Collage.filled,$Color.charcoal,A2($Graphics$Collage.ngon,3,81))))]));
+                                             {ctor: "_Tuple2",_0: _p8,_1: -50 + _p9},
+                                             A2($Graphics$Collage.filled,newColor,A2($Graphics$Collage.ngon,3,radius))))
+                                             ,ribbon]));
    };
-   var bShape = balloon({ctor: "_Tuple2",_0: -20,_1: -100});
-   var aBalloon = {x: -20,y: -100,b: bShape};
-   var main = A2($Signal.map,view,A3($Signal.foldp,update,aBalloon,iter));
-   var Loon = F3(function (a,b,c) {    return {x: a,y: b,b: c};});
-   return _elm.Main.values = {_op: _op,Loon: Loon,balloon: balloon,bShape: bShape,aBalloon: aBalloon,view: view,update: update,iter: iter,main: main};
+   var bShape = balloon({ctor: "_Tuple2",_0: -20,_1: 0});
+   var aBalloon = {x: -20,y: 0,b: bShape,vx: 0,vy: 0};
+   var bBalloon = {x: 70,y: 10,b: balloon({ctor: "_Tuple2",_0: 80,_1: 10}),vx: 0,vy: 0};
+   var simulation = {allLoons: _U.list([aBalloon,bBalloon]),deltaSum: 0};
+   var view = F2(function (_p10,sim) {
+      var _p11 = _p10;
+      var _p12 = _p11._1;
+      var allLoons = A2($List.map,function (_) {    return _.b;},sim.allLoons);
+      var gameScale = A2($Basics.min,$Basics.toFloat(_p12) / baseHeight,1);
+      return A3($Graphics$Collage.collage,_p11._0,_p12,A2($List.map,$Graphics$Collage.scale(gameScale),allLoons));
+   });
+   var updateLoon = F2(function (dt,loon) {
+      var delta = dt / 100;
+      return _U.update(loon,
+      {x: loon.x
+      ,y: _U.cmp(loon.y,baseHeight - radius * 3) < 0 ? loon.y + loon.vy : loon.y
+      ,vx: loon.vx
+      ,vy: _U.cmp(loon.vy,20) < 0 ? loon.vy + delta : loon.vy
+      ,b: _U.cmp(loon.y,baseHeight - radius * 3) < 0 ? A2($Graphics$Collage.moveY,loon.vy,loon.b) : loon.b});
+   });
+   var updateLoons = F2(function (dt,sim) {
+      var balloons = function () {
+         var thisYSeed = $Random.initialSeed($Basics.ceiling(sim.deltaSum * 5));
+         var randYVal = getInt(runGen(thisYSeed));
+         var thisXSeed = $Random.initialSeed($Basics.floor(sim.deltaSum));
+         var randXVal = getInt(runGen(thisXSeed));
+         var newLoon = {x: $Basics.toFloat(randXVal)
+                       ,y: $Basics.toFloat(randYVal - baseHeight)
+                       ,vx: 0
+                       ,vy: 0
+                       ,b: balloon({ctor: "_Tuple2",_0: $Basics.toFloat(randXVal),_1: $Basics.toFloat(randYVal - baseHeight)})};
+         return _U.cmp(sim.deltaSum / $Basics.toFloat($List.length(sim.allLoons)),30) > 0 ? A2($List._op["::"],newLoon,sim.allLoons) : sim.allLoons;
+      }();
+      return _U.update(sim,{allLoons: A2($List.map,updateLoon(dt),balloons),deltaSum: sim.deltaSum + dt});
+   });
+   var simStep = A3($Signal.foldp,updateLoons,simulation,iter);
+   var runSim = A3($Signal.map2,view,$Window.dimensions,simStep);
+   var main = runSim;
+   var showSim = A2($Signal.map,showLoons,simStep);
+   var Sim = F2(function (a,b) {    return {allLoons: a,deltaSum: b};});
+   var Loon = F5(function (a,b,c,d,e) {    return {x: a,y: b,vx: c,vy: d,b: e};});
+   return _elm.Main.values = {_op: _op
+                             ,Loon: Loon
+                             ,Sim: Sim
+                             ,baseHeight: baseHeight
+                             ,baseWidth: baseWidth
+                             ,radius: radius
+                             ,getHeight: getHeight
+                             ,runGen: runGen
+                             ,genInitSeed: genInitSeed
+                             ,getSeed: getSeed
+                             ,getInt: getInt
+                             ,balloon: balloon
+                             ,bShape: bShape
+                             ,aBalloon: aBalloon
+                             ,bBalloon: bBalloon
+                             ,simulation: simulation
+                             ,toTuple: toTuple
+                             ,view: view
+                             ,updateLoon: updateLoon
+                             ,updateLoons: updateLoons
+                             ,showLoons: showLoons
+                             ,iter: iter
+                             ,toPair: toPair
+                             ,simStep: simStep
+                             ,runSim: runSim
+                             ,showSim: showSim
+                             ,main: main};
 };
